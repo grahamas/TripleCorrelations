a3190ab1223fc0f18fe5d3f7a79d5e91e54827dc
Graham Smith <grahamas@gmail.com>
2022-02-10 17:07:00 -0700

    export properly

--- src/TripleCorrelations.jl
+++ src/TripleCorrelations.jl
@@ -9,14 +9,9 @@ include("triple_correlation.jl")
 export TripleCorrelation, triple_correlation
 
 include("sequence_class.jl")
-export sequence_class_tricorr, 
-    sequence_class_tricorr_zeropad, 
-    sequence_class_tricorr_periodic
-export sequence_class_tricorr!, 
-    sequence_class_tricorr_zeropad!, 
-    sequence_class_tricorr_periodic!
-export sequence_class_tricorr_zeropad_unrolled,
-    sequence_class_tricorr_periodic_unrolled
+export sequence_class_tricorr, sequence_class_tricorr!
+export sequence_class_tricorr_unrolled,
+    sequence_class_tricorr_unrolled
 
 include("marginal.jl")
 export time_tricorr_zeropad, space_tricorr_zeropad, space_time_tricorr_zeropad,
