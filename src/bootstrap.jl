cdada57309fdfb0ab8cd77932c402435ab96b729
Graham Smith <grahamas@gmail.com>
2022-03-15 02:49:07 -0700

    Update bootstrap for PeriodicExtended

--- src/bootstrap.jl
+++ src/bootstrap.jl
@@ -43,10 +43,6 @@ end
 function bootstrap_sequence_classes_nonzero(raster::Array{Bool}, boundary, n_lag::Int, t_lag::Int, n_bootstraps::Int, bootstraps_step::Int)
     bootstrap_sequence_classes_nonzero(boundary, size(raster)..., count(raster), n_lag, t_lag, n_bootstraps, bootstraps_step)
 end
-function bootstrap_sequence_classes_nonzero(raster::Array{Bool}, boundary::PeriodicExtended, n_lag::Int, t_lag::Int, n_bootstraps::Int, bootstraps_step::Int)
-    @error "Unsupported--- handle extension shuffling."
-    bootstrap_sequence_classes_nonzero(boundary, size(raster)..., count(raster), n_lag, t_lag, n_bootstraps, bootstraps_step)
-end
 
 
 @memoize function bootstrap_sequence_classes_nonzero(boundary, n::Int, t::Int, count_ones::Int, n_lag::Int, t_lag::Int, n_bootstraps::Int, bootstraps_step::Int)
@@ -70,18 +66,41 @@ end
     return sequence_class_bootstrapped
 end
 
-function bootstrap_sequence_classes_nonzero(arr::AbstractArray{<:AbstractFloat}, boundary::PeriodicExtended, n_lag::Int, t_lag::Int, n_bootstraps::Int, bootstraps_step::Int)
-    @error "Unimplemented--- need to shuffle inner"
-    inplace_arr = copy(arr)
+@memoize function bootstrap_sequence_classes_nonzero(boundary::PeriodicExtended, n::Int, t::Int, count_ones::Int, n_lag::Int, t_lag::Int, n_bootstraps::Int, bootstraps_step::Int)
     max_bootstraps = n_bootstraps * 20
-    sequence_class_bootstrapped = bootstrap_sequence_classes!(inplace_arr, boundary, n_lag, t_lag, n_bootstraps) .* n_bootstraps
+    unshuffled_raster = zeros(Bool, n, t)
+    unshuffled_raster[1:count_ones] .= 1
+    sequence_class_bootstrapped = bootstrap_sequence_classes!(unshuffled_raster, boundary, n_lag, t_lag, n_bootstraps) .* n_bootstraps
+    t0, t1 = boundary.t_bounds
     while any(sequence_class_bootstrapped .== 0) && (n_bootstraps < max_bootstraps)
         @warn "insufficient n_bootstraps = $n_bootstraps [(n,t) = $((n,t)); lag = $((n_lag,t_lag))]"
         n_bootstraps += bootstraps_step
-        sequence_class_bootstrapped += sum(
-            sequence_class_tricorr(shuffle!(unshuffled_raster), boundary, n_lag, t_lag) 
-                for _ ∈ 1:bootstraps_step
-        )
+        
+        sequence_class_bootstrapped += mapreduce(+, 1:bootstraps_step) do _
+            shuffle!(@view inplace_arr[:, t0:t1])
+            sequence_class_tricorr(inplace_arr, boundary, n_lag, t_lag)
+        end
+    end
+    if n_bootstraps >= max_bootstraps
+        @warn "BAD"
+        @show boundary n t count_ones n_lag t_lag
+    end
+    sequence_class_bootstrapped ./= n_bootstraps
+    return sequence_class_bootstrapped
+end
+
+function bootstrap_sequence_classes_nonzero(arr::AbstractArray{<:AbstractFloat}, boundary::PeriodicExtended, n_lag::Int, t_lag::Int, n_bootstraps::Int, bootstraps_step::Int)
+    inplace_arr = copy(arr)
+    max_bootstraps = n_bootstraps * 20
+    sequence_class_bootstrapped = bootstrap_sequence_classes!(inplace_arr, boundary, n_lag, t_lag, n_bootstraps) .* n_bootstraps
+    t0, t1 = boundary.t_bounds
+    while any(sequence_class_bootstrapped .== 0) && (n_bootstraps < max_bootstraps)
+        @warn "insufficient n_bootstraps = $n_bootstraps [(n,t) = $((n,t)); lag = $((n_lag,t_lag))]"
+        n_bootstraps += bootstraps_step
+        sequence_class_bootstrapped += mapreduce(+, 1:bootstraps_step) do _
+            shuffle!(@view inplace_arr[:, t0:t1])
+            sequence_class_tricorr(inplace_arr, boundary, n_lag, t_lag)
+        end
     end
     if n_bootstraps >= max_bootstraps
         @warn "Degenerate array or something (all zeros)."
@@ -98,7 +117,7 @@ function bootstrap_sequence_classes_nonzero(arr::AbstractArray{<:AbstractFloat},
         @warn "insufficient n_bootstraps = $n_bootstraps [(n,t) = $((n,t)); lag = $((n_lag,t_lag))]"
         n_bootstraps += bootstraps_step
         sequence_class_bootstrapped += sum(
-            sequence_class_tricorr(shuffle!(unshuffled_raster), boundary, n_lag, t_lag) 
+            sequence_class_tricorr(shuffle!(inplace_arr), boundary, n_lag, t_lag) 
                 for _ ∈ 1:bootstraps_step
         )
     end
